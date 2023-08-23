fd43004b3f19d15f6457d2f2d56b3a26a4ebbf47
appledog <45581427+applejwjcat@users.noreply.github.com>
2023-08-23 13:39:13 +0800

    Update wsl_arch.sh

--- wsl_arch.sh
+++ wsl_arch.sh
@@ -1,3 +1,3 @@
-sudo pacman -S neovim ctags ranger zsh vim ripgrep nodejs npm python-pip python-neovim the_siliver_search proxychains-ng perl xclip ueberzug bat fd bash-language-server
+sudo pacman -S neovim ctags ranger zsh vim ripgrep nodejs npm python-pip python-neovim the_silver_searcher proxychains-ng perl xclip ueberzug bat fd bash-language-server
 npm install -g neovim
 
